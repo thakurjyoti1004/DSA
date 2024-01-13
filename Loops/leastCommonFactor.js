444a10db86ce884921153f22ef169d511f0cb67e
jyoti <jthakur777.jt@gmail.com>
2024-01-13 11:22:26 -0800

    added least common factor function

--- Loops/leastCommonFactor.js
+++ Loops/leastCommonFactor.js
@@ -1,11 +1,13 @@
-let a = 9;
-let b = 12;
-let gcd = 0;
+function leastCommonfactor(a, b) {
+  let gcd = 0;
 
-for (let i = 1; i <= a && b; i++) {
-  if (a % i == 0 && b % i == 0) {
-    gcd = i;
+  for (let i = 1; i <= a && b; i++) {
+    if (a % i == 0 && b % i == 0) {
+      gcd = i;
+    }
   }
+  let lcd = (a * b) / gcd;
+  return lcd;
 }
-let lcd = (a * b) / gcd;
-console.log(lcd);
+
+console.log(leastCommonfactor(9, 12));
