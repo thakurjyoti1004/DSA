815ec189a2e65323608bb112dae2445d58019f2d
jyoti <jthakur777.jt@gmail.com>
2024-01-13 11:26:07 -0800

    added least common factor function

--- Loops/leastCommonFactor.js
+++ Loops/leastCommonFactor.js
@@ -1,7 +1,7 @@
 function leastCommonfactor(a, b) {
   let gcd = 0;
 
-  for (let i = 1; i <= a && b; i++) {
+  for (let i = 1; i <= Math.min(a, b); i++) {
     if (a % i == 0 && b % i == 0) {
       gcd = i;
     }
