d89bde25f26d1f5408731e376f2d9e7bfb495200
jyoti <jthakur777.jt@gmail.com>
2024-05-17 16:45:46 +0530

    single number problem by Bitwise XOR

---
+++ problemsOnArrays/single_number_XOR.js
@@ -0,0 +1,9 @@
+function singleNum(arr) {
+  let res = 0;
+  for (let i = 0; i < arr.length; i++) {
+    res = res ^ arr[i];
+  }
+  return res;
+}
+const finalResult = singleNum([4, 3, 2, 3, 4]);
+console.log(finalResult);
